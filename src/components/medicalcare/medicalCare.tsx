b38e80482d1d7bb1b69ff9d761b9661a6da61c2d
mashaheeer <michel.nassar@itxi.net>
2022-11-25 15:46:52 +0200

    Finished medical care section on wide screens

--- src/components/medicalcare/medicalCare.tsx
+++ src/components/medicalcare/medicalCare.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from "react";
 import './medicalCareStyles.css'
 import cosmeticImg from '../../assets/Images/cosmetic.jpg'
-
+import pediatric from '../../assets/Images/pediatric.webp'
+import ortho from '../../assets/Images/ortho.jpg'
+import dentalImplant from '../../assets/Images/dentalimplant.jpg'
+import pain from '../../assets/Images/pain.jpg'
 export const MedicalCare:FC<any> = (props) =>{
     return  <div className="medicalcare-container">
         <div className="medicalcare-square">
@@ -10,6 +13,7 @@ export const MedicalCare:FC<any> = (props) =>{
             <h1>Providing Medical Care For The Sickest In Our Community</h1>
         </div>
         <div className="medicalcare-bottom-container">
+            <div className="medicalcare-bottom-top-sec">
             <div className="medicalcare-item-container">
                 <div className="medicalcare-img-container">
                 <img src={cosmeticImg} alt='Cosmetic'/>
@@ -18,6 +22,45 @@ export const MedicalCare:FC<any> = (props) =>{
                     <h1>Cosmestic Dentistry</h1>
                     <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog MTV quiz graced</p>
                 </div>
+            </div>
+            <div className="medicalcare-item-container">
+                <div className="medicalcare-img-container">
+                <img src={dentalImplant} alt='dental implant'/>
+                </div>
+                <div className="medicalcare-text-container">
+                    <h1>Dental Implant</h1>
+                    <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog MTV quiz graced</p>
+                </div>
+            </div>
+            <div className="medicalcare-item-container">
+                <div className="medicalcare-img-container">
+                <img src={pain} alt='pain mgt'/>
+                </div>
+                <div className="medicalcare-text-container">
+                    <h1>Pain Management</h1>
+                    <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog MTV quiz graced</p>
+                </div>
+            </div>
+            </div>
+            <div className="medicalcare-bottom-bottom-sec">
+            <div className="medicalcare-item-container">
+                <div className="medicalcare-img-container">
+                <img src={ortho} alt='ortho'/>
+                </div>
+                <div className="medicalcare-text-container">
+                    <h1>Orthodontic</h1>
+                    <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog MTV quiz graced</p>
+                </div>
+            </div>
+            <div className="medicalcare-item-container">
+                <div className="medicalcare-img-container">
+                <img src={pediatric} alt='Cosmetic'/>
+                </div>
+                <div className="medicalcare-text-container">
+                    <h1>Pediatric</h1>
+                    <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog MTV quiz graced</p>
+                </div>
+            </div>
             </div>
         </div>
         </div>
